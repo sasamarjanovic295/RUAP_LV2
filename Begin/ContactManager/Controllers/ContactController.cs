e41aa32733a69ed63b17fc29580580df3113194d
Sasa Marjanovic <sasa.marjanovic295@gmail.com>
2023-11-23 00:30:25 +0100

    Extracting Functionality into a Service Layer

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ContactManager.Models;
+using ContactManager.Services;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,27 +12,16 @@ namespace ContactManager.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private ContactRepository contactRepository;
+
+        public ContactController()
         {
-            return View();
+            this.contactRepository = new ContactRepository();
         }
 
         public Contact[] Get()
         {
-            return new Contact[]
-            {
-                new Contact
-                {
-                    Id = 1,
-                    Name = "Glenn Block"
-                },
-                new Contact
-                {
-                    Id = 2,
-                    Name = "Dan Roth"
-                }
-            };
+            return contactRepository.GetAllContacts();
         }
     }
 }
