b4a4d78e9dd35b3fcb6164db1b189af3eef5e844
Sasa Marjanovic <sasa.marjanovic295@gmail.com>
2023-11-23 00:15:11 +0100

    Creating the API Project

---
+++ Begin/ContactManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
